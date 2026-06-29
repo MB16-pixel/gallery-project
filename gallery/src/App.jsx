ea5b38eb6e87085615ab50020315e49ccdda5d54
MB16-pixel <mistedmedy@gmail.com>
2026-06-29 13:24:51 +0530

    Refactor App component: update button functionality, adjust data fetching limit, and enhance image display

--- gallery/src/App.jsx
+++ gallery/src/App.jsx
@@ -6,16 +6,28 @@ function App() {
   const [userData,setUserData] = useState([]);
 
   const getData = async () =>{
-    const response = await axios.get("https://picsum.photos/v2/list?page=2&limit=100");
+    const response = await axios.get("https://picsum.photos/v2/list?page=2&limit=15");
     setUserData(response.data);
   }
 
-  let 
+  let printUserData = "No data available";
+
+  if(userData.length>0){
+    printUserData = userData.map(function(elem,idx){
+      return(
+          <img className="images" src={elem.download_url} alt="" />
+      )
+    })
+  }
 
   return (
     <div className='container'>
       <button onClick={getData}>Get data</button>
+      <div  className='picture'>
+        {printUserData}
+      </div>
     </div>
+
   )
 }
 
