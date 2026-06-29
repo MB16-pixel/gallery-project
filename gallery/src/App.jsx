ebb9423a29032469a5bc06d3fd49fcc61afdd652
MB16-pixel <mistedmedy@gmail.com>
2026-06-29 12:42:58 +0530

    first commit

---
+++ gallery/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import axios from 'axios'
+function App() {
+  
+
+  return (
+    <div className='container'>
+      <button>Get data</button>
+    </div>
+  )
+}
+
+export default App
