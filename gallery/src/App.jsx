bcdd40303869d00557274fad90fb82c98d9a6709
MB16-pixel <mistedmedy@gmail.com>
2026-06-29 13:11:49 +0530

    Update App component and styles: change background colors and implement data fetching on button click

--- gallery/src/App.jsx
+++ gallery/src/App.jsx
@@ -1,11 +1,20 @@
+import { useState } from 'react'
 import './App.css'
 import axios from 'axios'
 function App() {
   
+  const [userData,setUserData] = useState([]);
+
+  const getData = async () =>{
+    const response = await axios.get("https://picsum.photos/v2/list?page=2&limit=100");
+    setUserData(response.data);
+  }
+
+  let 
 
   return (
     <div className='container'>
-      <button>Get data</button>
+      <button onClick={getData}>Get data</button>
     </div>
   )
 }
