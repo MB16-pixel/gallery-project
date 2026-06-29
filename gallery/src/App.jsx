047a1c3c83d6ceddfbb39a25417956f7d5b45605
MB16-pixel <mistedmedy@gmail.com>
2026-06-29 13:53:34 +0530

    Refactor App component: implement data fetching on mount, update image display with links, and adjust button layout

--- gallery/src/App.jsx
+++ gallery/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import axios from 'axios'
 function App() {
@@ -8,23 +8,34 @@ function App() {
   const getData = async () =>{
     const response = await axios.get("https://picsum.photos/v2/list?page=2&limit=15");
     setUserData(response.data);
+    console.log(response)
   }
+
+  useEffect(function(){
+    getData();
+  },[])
 
   let printUserData = "No data available";
 
   if(userData.length>0){
     printUserData = userData.map(function(elem,idx){
       return(
-          <img className="images" src={elem.download_url} alt="" />
+        <a href={elem.url} target='_blank'>
+          <img className="images" src={elem.download_url} alt="" key={idx} />
+        </a>
       )
     })
   }
 
   return (
     <div className='container'>
-      <button onClick={getData}>Get data</button>
+      {/* <button onClick={getData}>Get data</button> */}
       <div  className='picture'>
         {printUserData}
+      </div>
+      <div className='btn'>
+        <button >Back</button>
+        <button>Next</button>
       </div>
     </div>
 
